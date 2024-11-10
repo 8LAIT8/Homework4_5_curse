9a65ca669b009cbcaa66dee6cb6145c964629047
lilia <lilia@DESKTOP-64J80RS>
2024-11-10 17:07:58 +0200

    Добавьте файлы проекта.

---
+++ Homework4_3_curse.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+
+{
+
+	int a = 1, b = 1, c;
+
+	if (c = (a - b)) {
+
+    cout << "The value of c is: " << c;
+	}
+	else if (c == 0){
+		cout << "The value of c is: " << c;
+	}
+	return 0;
+
+}
